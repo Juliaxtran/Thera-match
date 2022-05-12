ec969082e416b87c0f16f28ba33d176d008311c2
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-12 15:47:30 -0400

    Add gitignore variables

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
