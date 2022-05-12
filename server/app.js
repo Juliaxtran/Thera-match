d57790d24e4e020f042811b39afc0c4d1b116520
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-12 14:26:52 -0400

    Database setup

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const PORT = 8000;
+const db = require('./configs/db.config');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,8 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter());
 
-app.listen(PORT, () => {
-  console.log(`Port running on ${PORT}`);
-})
+
 
 module.exports = app;
