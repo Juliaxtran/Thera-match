572d4e992a3a6618668ccd6322a30adb1bcdb606
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-12 14:53:26 -0400

    Add morgan and coors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const PORT = 8000;
 const db = require('./configs/db.config');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter());
