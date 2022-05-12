8ecd73272035054969c7ed1688f729f06ccc5dc0
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-05-12 19:30:03 -0400

    schema data

--- server/db/schema/03_specialties.sql
+++ server/db/schema/03_specialties.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS specialties CASCADE;
 
 CREATE TABLE specialties (
-  id SERIAL PRIMARY KEY REFERENCES therapist_specialties(specialty_id) ON DELETE CASCADE NOT NULL ,
+  id SERIAL PRIMARY KEY NOT NULL ,
   type VARCHAR(255) NOT NULL
 );
