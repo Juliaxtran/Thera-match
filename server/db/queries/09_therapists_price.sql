235127bd0439de47da5b0f6e8653298010dd8891
patriceandrea <andrea_pavico98@yahoo.ca>
2022-05-13 11:59:04 -0400

    query for cost per session

--- server/db/queries/09_therapists_price.sql
+++ server/db/queries/09_therapists_price.sql
@@ -1,7 +1,20 @@
+
+----- under cost_per_session_range 
+
 SELECT therapists.*, STRING_AGG(specialties.type,',') as type
 from therapists
   join therapist_specialties ON therapist_id = therapists.id
   join specialties ON specialties.id = therapist_specialties.specialty_id
-WHERE therapists.cost_per_session between 50 AND 100
+WHERE therapists.cost_per_session < $1
+GROUP BY therapists.id, therapists.first_name, therapists.last_name, therapists.email, therapists.phone_number, therapists.password,therapists.gender, therapists.image, therapists.date_of_birth, therapists.location, therapists.cost_per_session
+;
+
+------ over cost_per_session 
+
+SELECT therapists.*, STRING_AGG(specialties.type,',') as type
+from therapists
+  join therapist_specialties ON therapist_id = therapists.id
+  join specialties ON specialties.id = therapist_specialties.specialty_id
+WHERE therapists.cost_per_session > $1
 GROUP BY therapists.id, therapists.first_name, therapists.last_name, therapists.email, therapists.phone_number, therapists.password,therapists.gender, therapists.image, therapists.date_of_birth, therapists.location, therapists.cost_per_session
 ;
