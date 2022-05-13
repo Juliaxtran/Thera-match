5e2f2428e0b24b34828a32ef48a8f27773a81028
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-13 10:27:34 -0400

    Update specialities query

--- server/db/queries/02_specialties_for_therapist.sql
+++ server/db/queries/02_specialties_for_therapist.sql
@@ -1,3 +1,6 @@
 -- get all specialties for therapist with therapist id
 
-SELECT specialty_id from therapist_specialties where therapist_id = 1;
+SELECT type from
+specialties
+join therapist_specialties on specialty_id = specialties.id
+where therapist_id = 1;
