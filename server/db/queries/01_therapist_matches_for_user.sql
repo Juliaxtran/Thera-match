f1e9f1cbc06e1f6bacfcbd121a552234bb2f5905
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-12 21:50:06 -0400

    Add queries

---
+++ server/db/queries/01_therapist_matches_for_user.sql
@@ -0,0 +1,6 @@
+-- get therapist matches for user
+
+
+SELECT therapist_id from matches
+where user_id = 1;
+
---
+++ server/db/queries/02_specialties_for_therapist.sql
@@ -0,0 +1,3 @@
+-- get all specialties for therapist with therapist id
+
+SELECT specialty_id from therapist_specialties where therapist_id = 1;
