9d207334651c27851dc729d80dd3d7c51c66cae0
patriceandrea <andrea_pavico98@yahoo.ca>
2022-05-14 13:05:37 -0400

    fix bcrypt for login

--- server/routes/helpers.js
+++ server/routes/helpers.js
@@ -1,9 +1,8 @@
-const getUserByEmail = function (email, password, db) {
+const getUserByEmail = function (email, db) {
   const queryStringEmail = `SELECT *
   FROM users
-  WHERE email = $1
-  AND password = $2`
-  const values = [email, password]
+  WHERE email = $1`
+  const values = [email];
   return db
     .query(queryStringEmail, values)
     .then((result) => {
--- server/routes/users.js
+++ server/routes/users.js
@@ -13,40 +13,45 @@ module.exports = (db, dbQueries) => {
     }
 
 
-
-    dbQueries.getUserByEmail(email, password, db)
+    dbQueries.getUserByEmail(email, db)
       .then(user => {
-        if (user && bcrypt.compare(password, user.password)) {
-          req.session.userID = user.id;
-          return res.status(200).send('Login Succesful');
-        } else {
-          return res.status(401).send('No user found');
+        if (user) {
+          console.log('RD user', user.password);
+          bcrypt.compare(req.body.password, user.password, function (err, response) {
+            if (err) {
+              res.status(401).send('No user found');
+
+            }
+            req.session.userID = user.id;
+            res.status(200).send("Sucess");
+
+          })
         }
       })
       .catch(error => {
         console.log(error);
       });
-    });
+  });
 
-      router.post('/signup', (req, res) => {
+  router.post('/signup', (req, res) => {
 
-        let { first_name, last_name, email, password} = req.body
-        password = bcrypt.hashSync(password, 12);
-        const command = ' INSERT INTO users (first_name, last_name, email, password) VALUES($1, $2, $3, $4) RETURNING *;'
-        const values = [first_name, last_name, email, password]
-        db.query(command, values).then(data => {
+    let { first_name, last_name, email, password } = req.body
+    password = bcrypt.hashSync(password, 12);
+    const command = ' INSERT INTO users (first_name, last_name, email, password) VALUES($1, $2, $3, $4) RETURNING *;'
+    const values = [first_name, last_name, email, password]
+    db.query(command, values).then(data => {
 
 
-          if(data["rows"].length > 0) {
-            req.session.userId = data["rows"][0].id
-            console.log("id",  data["rows"][0].id )
-            return res.status(200).send("it worked")
-          }
+      if (data["rows"].length > 0) {
+        req.session.userId = data["rows"][0].id
+        console.log("id", data["rows"][0].id)
+        return res.status(200).send("it worked")
+      }
 
-        })
-        .catch((err) => console.log(err));
+    })
+      .catch((err) => console.log(err));
 
-      })
+  })
 
 
 
