13bfd05aaf30cb6563dfb2eba75f9393e4a64d30
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-15 12:12:15 -0400

    Add Front page header

---
+++ client/src/components/HomeNav.js
@@ -0,0 +1,12 @@
+
+
+const HomeNav = (props) => {
+  return (
+  <div className = "home-nav">
+    <img className = "logo" src = {`/images/other/logo.png`} alt="logo" />
+    <h1 className = "title"> Thera-Match</h1>
+  </div>
+  )
+  }
+
+  export default HomeNav
