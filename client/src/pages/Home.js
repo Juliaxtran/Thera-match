ae79e8f5ef1b65e7dc5a2aea68a34f1a783d9290
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-15 18:15:20 -0400

    Add Home css page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import HomeNav from "../components/HomeNav";
 import AuthModal from "../components/AuthModal";
 import { useState } from 'react'
+import '../Home.css';
 
 
 const Home = (props) => {
