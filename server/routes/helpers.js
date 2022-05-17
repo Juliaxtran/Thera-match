6bd88d5d859a6e88e280b5a7f21bc45eb2a88636
patriceandrea <andrea_pavico98@yahoo.ca>
2022-05-17 14:13:28 -0400

    add function to the backend

--- server/routes/helpers.js
+++ server/routes/helpers.js
@@ -42,7 +42,31 @@ const getTherapistByEmail = function (email, db) {
 }
 
 
+const getAllSpecialties = function (options) {
+  const queryParams = [];
+
+  let queryString =
+    `
+    SELECT therapists.first_name, 
+    STRING_AGG(specialties.type,',') as type
+    FROM therapists
+    JOIN therapist_specialties ON therapist_id = therapists.id
+    JOIN specialties ON specialties.id = therapist_specialties.specialty_id
+  `
+  if (options.type) {
+    queryParams.push(`%${options.type}%`);
+    queryString += `WHERE type LIKE $${queryParams.length}`
+  }
+
+  queryString +=
+    `
+    GROUP BY therapists.id, therapists.first_name, therapists.last_name, therapists.email, therapists.phone_number, therapists.password,therapists.gender, therapists.image, therapists.date_of_birth, therapists.location, therapists.cost_per_session, therapists.years_of_practice, therapists.title, therapists.session_type, therapists.about;
+    `
+  return pool.query(queryString, queryParams).then((res) => res.rows);
+}
+
 module.exports = {
   getUserByEmail,
-  getTherapistByEmail
+  getTherapistByEmail,
+  getAllSpecialties
 }
