4f70db4fef3d5e9f72692df22b59324fae8c2dda
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-16 20:19:20 -0400

    Change react port

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(cookieSession({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(cors({ origin: 'http://localhost:3000', methods: 'GET, POST', credentials: true }))
+app.use(cors({ origin: 'http://localhost:3002', methods: 'GET, POST', credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
