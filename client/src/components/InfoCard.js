b26338891702cf5e1dd271044614a3a381607192
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-19 19:13:08 -0400

    Fix therapist dashboard route error

---
+++ client/src/components/InfoCard.js
@@ -0,0 +1,25 @@
+// import axios from "axios"
+// import { useState, useEffect} from "react"
+
+
+
+
+// const InfoCard = () => {
+//   let [info, setInfo] = useState([]);
+
+//   useEffect(() => {
+//     axios.get('/therapists/show', { withCredentials: true }).then(res => {
+//       console.log("Data", res.data)
+//       setInfo(res.data);
+//     })
+//   }, [])
+
+
+//   return (
+//   <div>
+//  <h1>{info}</h1>
+//   </div>
+//   )
+//   }
+
+//   export default InfoCard
