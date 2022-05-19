07b240b42aa2d9724bc3dd2d1a13b2174cd8de83
patriceandrea <andrea_pavico98@yahoo.ca>
2022-05-18 22:21:45 -0400

    add data to the filter page

--- client/src/FilterTableItem.js
+++ client/src/FilterTableItem.js
@@ -1,58 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import FormGroup from '@mui/material/FormGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import axios from "axios";
 
+// const specialties = [
+//   {
+//     type: "Addiction",
+//   },
+//   {
+//     type: "Anxiety"
+//   },
+//   {
+//     type: "Binge Eating Disorder"
+//   },
+//   {
+//     type: "Bulimia"
+//   },
+//   {
+//     type: "Bullying"
+//   },
+//   {
+//     type: "Couples Therapy"
+//   },
+//   {
+//     type: "Depression"
+//   },
+//   {
+//     type: "Divorce"
+//   },
+//   {
+//     type: "Domestic Abuse"
+//   },
+//   {
+//     type: "Drug Abuse"
+//   },
+//   {
+//     type: "Family"
+//   },
+//   {
+//     type: "Grief"
+//   },
+//   {
+//     type: "LGBTQ2S+"
+//   },
+//   {
+//     type: "Sex Therapy"
+//   }
 
-const specialties = [
-  {
-    type: "Addiction",
-  },
-  {
-    type: "Anxiety"
-  },
-  {
-    type: "Binge Eating Disorder"
-  },
-  {
-    type: "Bulimia"
-  },
-  {
-    type: "Bullying"
-  },
-  {
-    type: "Couples Therapy"
-  },
-  {
-    type: "Depression"
-  },
-  {
-    type: "Divorce"
-  },
-  {
-    type: "Domestic Abuse"
-  },
-  {
-    type: "Drug Abuse"
-  },
-  {
-    type: "Family"
-  },
-  {
-    type: "Grief"
-  },
-  {
-    type: "LGBTQ2S+"
-  },
-  {
-    type: "Sex Therapy"
-  }
+// ];
 
-];
+// { specialties, setSpecialties }
 
 const FilterTableItem = ({ setSpecialties }) => {
-
   const [checked, setChecked] = useState([]);
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    axios.get('/filters/specialties')
+      .then(res => {
+        console.log('specialties', res.data)
+        setChecked(res.data);
+        setIssues(res.data);
+      })
+  }, [])
+
 
   const onChange = (isChecked, specialtyName) => {
     // console.log(isChecked);
@@ -82,11 +94,11 @@ const FilterTableItem = ({ setSpecialties }) => {
   return (
     <div className="filter-item">
       <FormGroup className='specialty-list'>
-        {specialties.map(specialty =>
+        {issues.map(specialty =>
           <FormControlLabel
-            key={specialty.type}
+            key={specialty.id}
             control={
-              <Checkbox checked={checked.includes(specialty.type)} onChange={(e) => onChange(e.target.checked, specialty.type)}
+              <Checkbox key={specialty.type} checked={checked.includes(specialty.type)} onChange={(e) => onChange(e.target.checked, specialty.type)}
               />
 
             }
