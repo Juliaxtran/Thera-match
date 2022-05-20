ea9f04ab4202c9891a91411b38d64a08f608240d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-05-20 09:10:53 -0400

    calendar booking in progress

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import axios from 'axios';
 
 // axios call to trigger appointment on selected day - check message input, similar
 // "I am interested in x date" msg template 
 
 
 
-export default function ReactCalendar() {
+export default function ReactCalendar(recipient) {
+
   const [selectDate, selectedDate] = useState("");
-  
   const recipient_id = recipient.user_id;
+
+  axios.post(`/messages`, { recipient_id, selectDate }, {withCredentials: true})
 
   const [date, setDate] = useState(new Date());
 
