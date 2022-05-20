d3620674889dd8f8847cfd137a51d110503086aa
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-19 20:21:27 -0400

    Add socket io server side

--- server/app.js
+++ server/app.js
@@ -5,6 +5,14 @@ const logger = require('morgan');
 const db = require('./configs/db.config');
 const cors = require('cors');
 const cookieSession = require('cookie-session');
+const server = require('./bin/www');
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    method: ["GET","POST"]
+  }
+});
+
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,6 +21,7 @@ const messagesRouter = require('./routes/messages');
 const therapistsRouter = require('./routes/therapists');
 const matchesRouter = require('./routes/matches');
 const filtersRouter = require('./routes/filters');
+
 
 const app = express();
 
@@ -25,7 +34,7 @@ app.use(cookieSession({
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(cors({ origin: 'http://localhost:3002', methods: 'GET, POST', credentials: true }))
+app.use(cors({ origin: '*', methods: 'GET, POST', credentials: true }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -55,5 +64,20 @@ app.get('/api/profile', (req, res) => {
     return res.status(400).send("No user info")
   }
 })
+
+io.on('connection', (socket) => {
+  socket.emit('me', socket.id);
+
+  socket.ion('disconnect', () => {
+    socket.broadcast.emit('callended');
+  })
+
+  socket.on("calluser", ({userToCall, signalData, from, name}) => {
+    io.to(userToCall).emit("calluser", {signal: signalData, from, name});
+  })
+  socket.on("answercall", (data) => {
+    io.to(data.to).emit("callaccepted", data.signal);
+  })
+});
 
 module.exports = app;
