0fe9668487a17a52e21d06c07fa88bb5b3a597be
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-22 13:38:18 -0400

    Clear input after submitting

--- server/db/schema/06_messages.sql
+++ server/db/schema/06_messages.sql
@@ -5,7 +5,7 @@ CREATE TABLE messages
 (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-  recipient_id integer REFERENCES users(id) ON DELETE CASCADE NOT NULL,
+  recipient_id integer REFERENCES users(id) ON DELETE CASCADE ,
   message text,
   created_at TIMESTAMP,
   conversation_id VARCHAR(250)
