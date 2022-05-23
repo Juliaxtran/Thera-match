ef184322e99b497209ddd6149e0189735037e07f
patriceandrea <andrea_pavico98@yahoo.ca>
2022-05-23 14:49:05 -0400

    add the message icon

--- client/src/pages/VideoChat.js
+++ client/src/pages/VideoChat.js
@@ -4,24 +4,37 @@ import Notification from "../components/Notifications";
 import Options from "../components/Options";
 import HomeNav from "../components/HomeNav";
 import '../stylesheet/VideoChat.css';
-
-
+import { Link } from "react-router-dom";
+import * as FaIcons from 'react-icons/fa';
+import { useContext } from "react";
+import UserContext from "../components/AppContext";
 
 const VideoChat = () => {
+
+  const { user } = useContext(UserContext);
 
   return (
     <div className="video-chat">
       <HomeNav />
       <AppBar position="static" color="inherit">
-        <h1 className="video-chat-header">Video Chat</h1>
+        <div className="message-to-chat">
+          <h3></h3>
+          <h1 className="video-chat-header">Video Chat</h1>
+          {user.type === "therapist" ? <Link to='/therapist-dashboard'>
+            <FaIcons.FaEnvelopeOpenText />
+          </Link> : <Link to='/messages'>
+            <FaIcons.FaEnvelopeOpenText />
+          </Link>}
+        </div >
       </AppBar>
+
       <div className="video-container">
         <VideoPlayer />
         <Options>
           <Notification />
         </Options>
       </div>
-    </div>
+    </div >
   )
 }
 
